00bcb86de070ece4e0b6202d762ab81a6ee79ea4
Nate Goldman <ungoldman@gmail.com>
2016-12-17 14:41:58 -0800

    add all exports to API contract test

--- test/index.js
+++ test/index.js
@@ -1,9 +1,11 @@
 var test = require('tape')
 var entypo = require('../')
 
-test('Example Test', function (t) {
-  t.plan(3)
+test('API contract', function (t) {
   t.equal(typeof entypo, 'function', 'main export is a  function')
   t.equal(typeof entypo.path, 'string', 'path exports a string')
   t.equal(typeof entypo.rawPath, 'string', 'rawPath exports a string')
+  t.equal(typeof entypo.htmlToElement, 'function', 'htmlToElement exports a function')
+  t.equal(typeof entypo.getNode, 'function', 'getNode exports a function')
+  t.end()
 })
